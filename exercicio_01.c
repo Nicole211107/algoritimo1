fb1645a2b9d563d7fa02a40e40d0432c2af80495
Nicole211107 <nicolesecorun21@gmail.com>
2025-07-23 13:46:41 -0300

    Update exercicio_01.c

--- exercicio_01.c
+++ exercicio_01.c
@@ -1,14 +1,26 @@
-/* Lê um valor real informado pelo usuário.
-Armazena o número com casas decimais.
-Exibe o valor formatado com duas casas. */
+/* O programa solicita três medidas ao usuário e verifica se elas podem formar um triângulo.
+Caso seja possível, identifica se o triângulo é equilátero, isósceles ou escaleno. 
+Se não puder formar um triângulo, exibe uma mensagem informando. */
 
 #include <stdio.h>
 
-   int main() {
-    float valor;
+int main() {
+    float x, y, z;
 
-    printf("Digite um valor real: ");
-    scanf("%f", &valor);
+    printf("Informe os três lados do triângulo:\n");
+    scanf("%f %f %f", &x, &y, &z);
 
-    printf("O valor digitado foi: %.2f\n", valor);
+    if (x + y > z && x + z > y && y + z > x) {
+        if (x == y && y == z) {
+            printf("Triângulo equilátero\n");
+        } else if (x == y || x == z || y == z) {
+            printf("Triângulo isósceles\n");
+        } else {
+            printf("Triângulo escaleno\n");
+        }
+    } else {
+        printf("As medidas informadas não formam um triângulo\n");
+    }
+
+    return 0;
 }
